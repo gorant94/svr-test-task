5ae3e313093512da408404d909f02338d799d062
Phillip Preobrazhenskiy <prof.preobrazhenskiy1925@gmail.com>
2019-09-03 12:35:12 +0300

    Add header, change controller, add styles

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,20 +1,6 @@
 'use strict';
 
-(function() {
-
-class MainController {
-
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
-
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
-}
-
 angular.module('svrTestTaskApp')
-  .controller('MainController', MainController);
+  .controller('MainController', function($scope, $http, $modal) {
 
-})();
+  });
